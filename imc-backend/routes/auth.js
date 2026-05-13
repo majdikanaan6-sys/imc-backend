2319da6f0efbd2b36053bc0dfd768060045b4646
Majdi Kanaan <majdikanaan6@gmail.com>
2026-05-13 12:08:44 -0700

    axious route

--- imc-backend/routes/auth.js
+++ imc-backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const pool = require("../db");
-
+const axios = require("axios");
 const router = express.Router();
 
 // ── AUTH MIDDLEWARE ──────────────────────────────────────────────────────────
